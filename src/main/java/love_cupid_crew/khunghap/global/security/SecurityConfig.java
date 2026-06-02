e20e8c89924a0ba7ca87db419f22e40dbf5a3a5e
sunwoo <sswsjw1234@gmail.com>
2026-06-02 17:27:02 +0900

    feat: 로그인, 로그아웃, refresh 구현

--- src/main/java/love_cupid_crew/khunghap/global/security/SecurityConfig.java
+++ src/main/java/love_cupid_crew/khunghap/global/security/SecurityConfig.java
@@ -33,7 +33,10 @@ public class SecurityConfig {
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
-                                "/api/auth/**",
+                                "/api/auth/email/**",
+                                "/api/auth/register",
+                                "/api/auth/login",
+                                "/api/auth/refresh",
                                 "/swagger-ui/**",
                                 "/swagger-ui.html",
                                 "/v3/api-docs/**"
